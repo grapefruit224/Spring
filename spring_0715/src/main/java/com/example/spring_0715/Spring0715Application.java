2f2106db66ae893618ae83019b230b542f0d501f
grapefriut422 <prikey7@gmail.com>
2024-07-16 17:43:47 +0900

    0716 - modify 기능 추가!

---
+++ spring_0715/src/main/java/com/example/spring_0715/Spring0715Application.java
@@ -0,0 +1,30 @@
+package com.example.spring_0715;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication(scanBasePackages = "com.example.spring_0715.day0716")
+public class Spring0715Application {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @PostConstruct
+    public void printConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("포트번호: " + port);
+        System.out.println("애플리케이션 이름: " + appName);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0715Application.class, args);
+    }
+
+}
---
+++ spring_0715/src/test/java/com/example/spring_0715/Spring0715ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_0715;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0715ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
